299a88c14f7ea1a2b266bffbc3b63856a8a15ce6
Anan Wang <mengan.wang@gmail.com>
2021-11-30 15:26:52 -0800

    homepage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import RequestSwipe from './components/RequestSwipe';
+import SearchSwipe from './components/SearchSwipe';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <RequestSwipe />
+        <SearchSwipe />
       </header>
     </div>
   );
