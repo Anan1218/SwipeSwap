0fe5e19398c06894add290f297175264db23dab5
Anan Wang <mengan.wang@gmail.com>
2021-11-30 15:26:18 -0800

    search display working

---
+++ src/components/SearchSwipe.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+
+import { firebase } from "../services/Firebase";
+import { getFirestore, collection, getDocs} from 'firebase/firestore/lite';
+
+import SwipeList from "./SwipeList";
+
+const db = getFirestore(firebase);
+
+function SearchSwipe() {
+  const [swipes, setSwipes] = useState([]);
+
+  useEffect(() => {
+    const fetchSwipes = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "BuySwipe"));
+        querySnapshot.forEach((doc) => {
+          setSwipes(requests => [...requests, doc.data()]);
+        });
+        // console.log(swipes);
+      } catch (e) {
+        console.log(e);
+      } 
+    }
+    fetchSwipes();
+  }, []);
+  return (
+    <div>
+      <SwipeList swipes={swipes}/>
+    </div>
+  );
+};
+
+export default SearchSwipe;
