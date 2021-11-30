bb932eff1fb70b8aa9e4b3d7d876a147483b1762
Tony Jiang <tonyjiang02@gmail.com>
2021-11-29 23:40:58 -0800

    firebase stuffs

---
+++ src/services/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCjXYqwNdwAb3Z-4yHuozlRB30sKvFc4b0",
+    authDomain: "swipeswap-ec1f0.firebaseapp.com",
+    projectId: "swipeswap-ec1f0",
+    storageBucket: "swipeswap-ec1f0.appspot.com",
+    messagingSenderId: "594857878325",
+    appId: "1:594857878325:web:7705ac4eee924e73cce27c"
+};
+
+// Initialize Firebase
+export const firebase = initializeApp(firebaseConfig);
