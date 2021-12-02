c8187c4ca081ccabe873d44455b9ee7ca490c9b5
Sraavya Pradeep <46724697+SraavyaPradeep@users.noreply.github.com>
2021-12-01 19:19:11 -0800

    Add files via upload

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function Navigation() {
+  return (
+    <div className="navigation">
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <div className="container">
+          <NavLink className="navbar-brand" to="/">
+            React Multi-Page Website
+          </NavLink>
+          <div>
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/">
+                  Home
+                  <span className="sr-only">(current)</span>
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/about">
+                  About
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/contact">
+                  Contact
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/blog">
+                  Blog
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navigation;import React from 'react';
+import logo from '../images/logo.png';
+import SignIn from './SignIn';
+export default function Landing() {
+    return (
+        <div className="mainBackground">
+            <img src={logo} style={{ height: "100px" }}></img>
+            <h1 class="lead" style={{ color: "white" }}>Swipe Swap: A program to help you find swipes at UCLA.</h1>
+            <SignIn />
+        </div >
+    );
+}
