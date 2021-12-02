622646f26583b4e5367c8a6750e50edae3c2dbf9
Sraavya Pradeep <46724697+SraavyaPradeep@users.noreply.github.com>
2021-12-01 21:28:23 -0800

    Add files via upload

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,28 +2,32 @@ import React from "react";
 
 function Home() {
   return (
+	
     <div className="home">
+		  
       <div class="container">
-        <div class="row align-items-center my-5">
+        <div class="row align-items-center my-5" >
           <div class="col-lg-7">
-            <img
-              class="img-fluid rounded mb-4 mb-lg-0"
-              src="http://placehold.it/900x400"
-              alt=""
-            />
+            <img class="img-fluid rounded mb-4 mb-lg-0" src="./../images/Ee36c0VUEAU-_sZ.jpeg" alt=""/>
           </div>
           <div class="col-lg-5">
-            <h1 class="font-weight-light">Home</h1>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
-            </p>
+            <h1 class="font-weight-light">Swipe Swap</h1>
+			<div class="homepara">
+				<p>
+			  Gain access to UCLA’s diverse dining options in a few clicks. Swipe swap is an easy way to share meal swipes between on & off campus students.
+				</p>
+			</div>
           </div>
         </div>
       </div>
+		  <div class="after">
+				
+		  </div>
+		  <div class="bottom">
+				
+		  </div>
     </div>
+	
   );
 }
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,32 +4,17 @@ import { NavLink } from "react-router-dom";
 function Navigation() {
   return (
     <div className="navigation">
-      <nav className="navbar navbar-expand navbar-dark bg-dark">
+      <nav className="navbar navbar-expand">
         <div className="container">
           <NavLink className="navbar-brand" to="/">
-            React Multi-Page Website
+            UCLA Swipe Swap
           </NavLink>
           <div>
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
                 <NavLink className="nav-link" to="/">
                   Home
-                  <span className="sr-only">(current)</span>
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/about">
-                  About
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/contact">
-                  Contact
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/blog">
-                  Blog
+                  <span className="sr-only"></span>
                 </NavLink>
               </li>
             </ul>
